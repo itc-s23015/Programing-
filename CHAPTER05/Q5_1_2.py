6903f4677d69902897a72fcc2c049b9605eb3be3
Yuu_Tamashiro <s23015@std.it-college.ac.jp>
2023-08-08 05:52:10 +0000

    add 5-1-1, 5-1-2, 5-1-3

---
+++ CHAPTER05/Q5_1_2.py
@@ -0,0 +1,8 @@
+stack = []
+stack.append(1)
+print(stack)
+stack.append(2)
+print(stack)
+print(stack.pop(), "を取り出す")
+print(stack.pop(), "を取り出す")
+print(stack)
---
+++ CHAPTER05/Q5_1_3.py
@@ -0,0 +1,8 @@
+queue = []
+queue.append(1)
+print(queue)
+queue.append(2)
+print(queue)
+print(queue.pop(0), "を取得")
+print(queue.pop(0), "を取得")
+print(queue)
