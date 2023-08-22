a01e7879f15eb32115583ead0bdb89509e37c0ba
Yuu_Tamashiro <s23015@std.it-college.ac.jp>
2023-08-22 07:04:37 +0000

    5syousyuuryou

---
+++ CHAPTER05/Q5_9_1.py
@@ -0,0 +1,11 @@
+moutain_in_japan = {
+    "fuji": 3776,
+    "kitadake": 3193,
+    "okuhodakadake": 3190,
+    'dummy': 0
+}
+
+moutain_in_japan_sorted = sorted(
+    moutain_in_japan.items(), key=lambda x: x[1], reverse=True
+)
+print(moutain_in_japan_sorted)
